db8101f1422b3206df8fdf05435dcaa2c9bbd0df
Juan-LukeKlopper <luke@stakeordie.com>
2022-10-25 01:14:56 +0200

    feat: Add starter React and Svelte projects

---
+++ buildspace/build-solana-pay-store/solana-pay-starter-main_svelte/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Head from './components/head.svelte';
+</script>
+
+<main>
+  <Head />
+  <h1>Custom Solana Store</h1>
+
+  <p>
+    My first online store that accepts payments with Solana or USDC
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
